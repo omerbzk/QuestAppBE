2df3dda7a4fc231086f80d71ea558bb076dbd6fa
omerbzk <bozkurtomer150@gmail.com>
2025-03-14 17:57:08 +0300

    JWT Security is impl.

---
+++ src/main/java/com/project/questapp/security/JwtTokenProvider.java
@@ -0,0 +1,27 @@
+package com.project.questapp.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+
+    @Value("${questapp.app.secret}")
+    private String APP_SECRET;
+    @Value("${questapp.app.expires.in}")
+    private Long EXPIRES_IN;
+
+    public String generateJwtToken(Authentication auth) {
+        JwtUserDetails userDetails = (JwtUserDetails) auth.getPrincipal();
+        Date expireDate = new Date(new Date().getTime() + EXPIRES_IN);
+        return Jwts.builder().setSubject(Long.toString(userDetails.getId()))
+                .setIssuedAt(new Date()).setExpiration(expireDate)
+                .signWith(SignatureAlgorithm.HS512, APP_SECRET).compact();
+    }
+
+}
